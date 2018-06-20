4b1594177eae3a14a60d8f6008c673840b0dd4be
Akash Gopala Bangera <akash@vivaiolab.com>
2018-06-21 01:35:53 +0200

    created basic pages

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {Question1Component} from './question1/question1.component'
+import {Question1Component} from './question1/question1.component';
+import {Question2Component} from './question2/question2.component';
+import {Question3Component} from './question3/question3.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component:HomeComponent, data: { state: 'home'}  },
     {path : 'question1', component : Question1Component, data: { state: 'question1'}},
+    {path : 'question2', component : Question2Component, data: { state: 'question2'}},
+    {path : 'question3', component : Question3Component, data: { state: 'question3'}},
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
