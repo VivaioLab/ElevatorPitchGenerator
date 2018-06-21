1c02cc1024e421814d414f50824cbd988eae1b7a
Akash Gopala Bangera <akash@vivaiolab.com>
2018-06-21 19:48:01 +0200

    Thank you page

---
+++ src/app/thankyou/thankyou.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormArray, FormBuilder, FormGroup,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-thankyou',
+  templateUrl: './thankyou.component.html',
+  styleUrls: ['./thankyou.component.css']
+})
+export class ThankyouComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
