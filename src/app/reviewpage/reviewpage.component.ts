b3579d58f562a2bbd9a3aa45aa457b4a3980e732
Akash Gopala Bangera <akash@vivaiolab.com>
2018-06-25 13:01:16 +0200

    email page

--- src/app/reviewpage/reviewpage.component.ts
+++ src/app/reviewpage/reviewpage.component.ts
@@ -22,25 +22,5 @@ question5 : string;
     this.question4 = localStorage.getItem('question4');
     this.question5 = localStorage.getItem('question5');
   }
-  saveChanges(){
-    
-    
-
-    localStorage.setItem('question1', JSON.stringify(this.question1) );
-    console.log(localStorage.getItem('question1'));
-
-    localStorage.setItem('question2', JSON.stringify(this.question2 ));
-    console.log(localStorage.getItem('question2'));
-
-    localStorage.setItem('question3', JSON.stringify(this.question3) );
-    console.log(localStorage.getItem('question3'));
-
-    localStorage.setItem('question4', JSON.stringify(this.question4) );
-    console.log(localStorage.getItem('question4'));
-
-    localStorage.setItem('question5', JSON.stringify(this.question5) );
-    console.log(localStorage.getItem('question5'));
-   
-    
-  }
+  
 }
