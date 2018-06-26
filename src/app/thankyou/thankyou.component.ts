d4b8cb205d47b4acd86eee93377ed4106ce891b1
Akash Gopala Bangera <akash@vivaiolab.com>
2018-06-26 18:00:48 +0200

    Added email functionality

--- src/app/thankyou/thankyou.component.ts
+++ src/app/thankyou/thankyou.component.ts
@@ -13,6 +13,7 @@ import { DomSanitizer } from "@angular/platform-browser";
 export class ThankyouComponent implements OnInit {
 
   modalRef: BsModalRef;
+  ques:string;
   constructor(private modalService: BsModalService, private matIconRegistry: MatIconRegistry,private domSanitizer: DomSanitizer) { 
     // this.matIconRegistry.addSvgIcon(
     //   "elevetorly",
@@ -25,6 +26,7 @@ export class ThankyouComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.ques="Ciao, My name is "+ localStorage.getItem('question1') +"I got really frustrated when "+ localStorage.getItem('question2')+"With our product "+ localStorage.getItem('question3')+"Ultimately the biggest value for the customer is that "+ localStorage.getItem('question4')+"Right now "+ localStorage.getItem('question5');
   }
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
