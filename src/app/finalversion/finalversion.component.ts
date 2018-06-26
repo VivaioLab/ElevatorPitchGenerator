3117019ad2e8e8eb77e512810874426363d16a43
Akash Gopala Bangera <akash@vivaiolab.com>
2018-06-26 16:53:01 +0200

    Added the proper flow and design

--- src/app/finalversion/finalversion.component.ts
+++ src/app/finalversion/finalversion.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
 
 @Component({
+  
   selector: 'app-finalversion',
   templateUrl: './finalversion.component.html',
-  styleUrls: ['./finalversion.component.css']
+  styleUrls: ['./finalversion.component.css'],
+  exportAs: 'child'
 })
 export class FinalversionComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { 
+    
+  }
+  saveChanges()
+  {
+    
+  }
 
+  
   ngOnInit() {
   }
 
+  
+
 }
+
--- src/app/thankyou/thankyou.component.ts
+++ src/app/thankyou/thankyou.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { FormControl, FormArray, FormBuilder, FormGroup,Validators } from '@angular/forms';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-thankyou',
@@ -12,7 +13,16 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 export class ThankyouComponent implements OnInit {
 
   modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) { }
+  constructor(private modalService: BsModalService, private matIconRegistry: MatIconRegistry,private domSanitizer: DomSanitizer) { 
+    // this.matIconRegistry.addSvgIcon(
+    //   "elevetorly",
+    //   this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/elevetorlylogo.svg")
+    // );
+    this.matIconRegistry.addSvgIcon(
+      "thanks",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/thanks.svg")
+    );
+  }
 
   ngOnInit() {
   }
