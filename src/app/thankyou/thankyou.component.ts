18033bde0b11181cd573d4f2a1610893d6dc6776
Akash Gopala Bangera <akash@vivaiolab.com>
2018-06-27 11:25:51 +0200

    Added side nav bar

--- src/app/thankyou/thankyou.component.ts
+++ src/app/thankyou/thankyou.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef,ViewChild } from '@angular/core';
 import { FormControl, FormArray, FormBuilder, FormGroup,Validators } from '@angular/forms';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
+import{CdkTextareaAutosize} from "@angular/cdk/text-field";
 
 @Component({
   selector: 'app-thankyou',
@@ -15,15 +16,23 @@ export class ThankyouComponent implements OnInit {
   modalRef: BsModalRef;
   ques:string;
   constructor(private modalService: BsModalService, private matIconRegistry: MatIconRegistry,private domSanitizer: DomSanitizer) { 
-    // this.matIconRegistry.addSvgIcon(
-    //   "elevetorly",
-    //   this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/elevetorlylogo.svg")
-    // );
+    this.matIconRegistry.addSvgIcon(
+      "elevetorly",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/elevetorlylogo.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "menu icon",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/menu icon.svg")
+    );
     this.matIconRegistry.addSvgIcon(
       "thanks",
       this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/thanks.svg")
     );
+    this.matIconRegistry.addSvgIcon(
+      "problem-icon",this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/problem icon.svg")
+    );
   }
+  @ViewChild('autosize') autosize : CdkTextareaAutosize;
 
   ngOnInit() {
     this.ques="Ciao, My name is "+ localStorage.getItem('question1') +"I got really frustrated when "+ localStorage.getItem('question2')+"With our product "+ localStorage.getItem('question3')+"Ultimately the biggest value for the customer is that "+ localStorage.getItem('question4')+"Right now "+ localStorage.getItem('question5');
