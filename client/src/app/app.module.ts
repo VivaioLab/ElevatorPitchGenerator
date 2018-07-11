0e76f44f0cda865db6cc754897b4c105080a18d9
Akash Gopala Bangera <akash@vivaiolab.com>
2018-07-11 12:38:37 +0200

    review page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,8 +20,6 @@ import {ModalModule} from 'ngx-bootstrap/modal';
 import { ConfirmpageComponent } from './confirmpage/confirmpage.component';
 import {MatToolbarModule,MatButtonModule,MatInputModule,MatIconModule, MatSidenavModule} from '@angular/material';
 import { FinalversionComponent } from './finalversion/finalversion.component';
-import { QuestionsComponent } from './questions/questions.component';
-import { Home2Component } from './home2/home2.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
 
@@ -39,8 +37,8 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     ThankyouComponent,
     ConfirmpageComponent,
     FinalversionComponent,
-    QuestionsComponent,
-    Home2Component,
+    // QuestionsComponent,
+    // Home2Component,
     SidenavComponent,
   ],
   imports: [
