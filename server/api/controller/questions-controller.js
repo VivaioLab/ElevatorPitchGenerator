cb28bae3a22a18c6fe570c12b91e42a2cca09873
jaynirmal1415 <jay@vivaiolab.com>
2018-08-01 15:28:33 +0200

    api email started

--- server/api/controller/questions-controller.js
+++ server/api/controller/questions-controller.js
@@ -7,7 +7,28 @@ const Login = db.login;
 const Pitch = db.pitch;
 const Answers = db.answers;
 
-
+exports.SendEmail = (req,res) =>{
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'jaynirmal15@gmail.com',
+          pass: 'hardik2010'
+        }
+      });
+      var mailOptions = {
+        from: 'jaynirmal15@gmail.com',
+        to: req.body.email,
+        subject: 'Sample Pitch',
+        text: 'That was easy!'
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
 
 exports.SaveAnswer = (req, res) => {	
 	// Save to PostgreSQL database
