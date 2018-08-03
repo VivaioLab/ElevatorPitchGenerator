c0a1a29ea8dc2b1aa51b2e4632b1dcd28e73f747
jaynirmal1415 <jay@vivaiolab.com>
2018-08-03 14:56:08 +0200

    modifications

---
+++ server/api/controller/login-controller.js
@@ -0,0 +1,40 @@
+'use strict';
+var generator = require('generate-password');
+var db = require('../../database/database');
+const login = db.login;
+const pitch = db.pitch;
+
+exports.createuser = (req, res) => {	
+    // Save to PostgreSQL database
+	login.create({
+        "user_name": req.body.email, 
+        "password": generator.generate({length : 8,numbers : true}), 
+        "email" : req.body.email,
+    }).then(login => {		
+			// Send created customer to client
+			res.json(login);
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({msg: "error", details: err});
+		});
+};
+
+exports.getusersbyemail = (req, res) => {
+	login.find({where :{email : req.body.email}}).then(login => {
+			// Send Login details to Client
+			res.json(login);
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({msg: "error", details: err});
+		});
+};
+
+exports.getusers = (req, res) => {
+	login.findAll().then(login => {
+			// Send All Users to Client
+			res.json(login.sort(function(c1, c2){return c1.id - c2.id}));
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({msg: "error", details: err});
+		});
+};
--- server/api/controller/questions-controller.js
+++ server/api/controller/questions-controller.js
@@ -7,43 +7,8 @@ const login = db.login;
 const pitch = db.pitch;
 const answers = db.answers;
 
-exports.sendemail = (req,res) =>{
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'jaynirmal15@gmail.com',
-          pass: 'hardik2010'
-        }
-      });
-      var mailOptions = {
-        from: 'jaynirmal15@gmail.com',
-        to: req.body.email,
-        subject: 'Sample Pitch',
-        text: 'That was easy!'
-      };
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-}
 
-exports.saveanswer = (req, res) => {	
-	// Save to PostgreSQL database
-	answers.create({
-        "answer": req.body.answer, 
-        "question_id": req.question_id, 
-        "pitch_id": req.body.pitch_id, 
-    }).then(questions => {		
-			// Send created customer to client
-			res.json(questions);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-		});
-};
+
 exports.createquestion = (req, res) => {	
 	// Save to PostgreSQL database
 	questions.create({
@@ -57,42 +22,7 @@ exports.createquestion = (req, res) => {
 			res.status(500).json({msg: "error", details: err});
 		});
 };
-exports.linkpitch = (req, res) => {	
-	// Save to PostgreSQL database
-	pitch.create({
-        "user_id": req.body.user_id
-    }).then(pitch => {		
-			// Send created customer to client
-			res.json(pitch);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-		});
-};
-exports.createuser = (req, res) => {	
-    // Save to PostgreSQL database
-    
-	login.create({
-        "user_name": req.body.email, 
-        "password": generator.generate({length : 8,numbers : true}), 
-        "email" : req.body.email,
-    }).then(login => {		
-			// Send created customer to client
-			res.json(login);
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-		});
-};
-exports.getusers = (req, res) => {
-	login.findAll().then(login => {
-			// Send All Customers to Client
-			res.json(login.sort(function(c1, c2){return c1.id - c2.id}));
-		}).catch(err => {
-			console.log(err);
-			res.status(500).json({msg: "error", details: err});
-		});
-};
+
 exports.getquestions = (req, res) => {
 	questions.findAll().then(question => {
 			// Send All Customers to Client
@@ -102,13 +32,23 @@ exports.getquestions = (req, res) => {
 			res.status(500).json({msg: "error", details: err});
 		});
 };
-eports.getanswers = (req,res) =>{
-    customer.findById(req.params.id).then(customer => {
-        res.json(customer);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({msg: "error", details: err});
-    });
-}
+exports.getquestionbyID = (req, res) => {	
+	questions.findById(req.body.id).then(question => {
+			res.json(question);
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({msg: "error", details: err});
+		});
+};
+exports.updatequestions = (req, res) => {
+	const id = req.body.id;
+	questions.update( req.body, 
+			{ where: {id: id} }).then(() => {
+				res.status(200).json(req.body);
+			}).catch(err => {
+				console.log(err);
+				res.status(500).json({msg: "error", details: err});
+			});
+};
 
 
